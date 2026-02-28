720ea26bc4fb5072d0bd7aef2fe1d4a96dbf7561
Marvellous <marvellousnwachukwu111@gmail.com>
2026-02-28 23:16:33 +0100

    add cnew page and changes

--- app/(session)/call/page.tsx
+++ app/(session)/call/page.tsx
@@ -8,23 +8,21 @@ import {
 } from '@stream-io/video-react-sdk';
 
 import '@stream-io/video-react-sdk/dist/css/styles.css';
+import { Button } from "@/components/ui/button";
+import { PhoneOff, Mic, MicOff, PictureInPicture2, Video, VideoOff } from 'lucide-react';
 
-const apiKey = "3krh3rxttsru";
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGVtby11c2VyLUlsOUUzT2hHIiwic3ViIjoidXNlci9kZW1vLXVzZXItSWw5RTNPaEciLCJhcGlLZXkiOiIza3JoM3J4dHRzcnUiLCJpYXQiOjE3NzIyNzE1MzEsImV4cCI6MTc3MjI3NTEzMX0.PYThZnA1iehLAHuEfzJvo2YLCAXx8QMszZ6TB2p7_SQ"; // This will be generated in the backend and passed here
-const userId = 'demo-user-NoWFbucN'; // This will be generated too
-const callId = 'demo-call-XrnP7Z-h'; // This will be generated
+import { useRouter } from "next/navigation";
 
-const user: User = { id: userId, name="Test" }
-if (!apiKey) {
-  throw new Error("apiKey is not found please add api key")
-}
-if (!token) {
-  console.log(token)
-  throw new Error("token is not found, please add token")
-}
 
-const client = new StreamVideoClient({ apiKey, user, token });
-const call = client.call('default', "demo-call-XrnP7Z-h");
+
+
+const apiKey = '3krh3rxttsru'
+const userId = 'demo-user-PUcymDzl'
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGVtby11c2VyLVBVY3ltRHpsIiwic3ViIjoidXNlci9kZW1vLXVzZXItUFVjeW1EemwiLCJhcGlLZXkiOiIza3JoM3J4dHRzcnUiLCJpYXQiOjE3NzIyOTAwNjcsImV4cCI6MTc3MjI5MzY2N30.6Q0So50LuIcQHimdxvrYBvaql6U0rlZpGYgnVa3bQJ0'
+
+const user: User = { id: userId }
+const client = new StreamVideoClient({ apiKey, user, token })
+const call = client.call('default', 'demo-call-lLh3nCHl')
 call.join({ create: true })
 
 export default function Stream() {
@@ -38,14 +36,14 @@ export default function Stream() {
 }
 
 export function UILayout() {
-  const call = useCall();
+  // const call = useCall();
   
   const {
     useCallCallingState, useParticipantCount,
     useLocalParticipant, useRemoteParticipants
   } = useCallStateHooks();
   
-  const localParticipant = useLocalParticipant();
+  // const localParticipant = useLocalParticipant();
   const callingState = useCallCallingState();
   // const participantCount = useParticipantCount();
   const remoteParticipants = useRemoteParticipants();
@@ -62,15 +60,78 @@ export function UILayout() {
         <MyParticipantList participants={remoteParticipants} />
         {/*<MyFloatingLocalParticipant participant={localParticipant} />*/}
       </StreamTheme>
+      <div className="absolute bottom-15 left-1/2 -translate-x-1/2 rounded-2xl">
+        <ButtonLayout />
+      </div>
     </div>
     
   );
 }
 
+export function ButtonLayout() {
+  const { useMicrophoneState, useCameraState } = useCallStateHooks();
+  const { microphone, isMute } = useMicrophoneState();
+  const { camera, isMute: isCameraMute } = useCameraState();
+  
+  const router = useRouter();
+  
+  async function microphoneToggle() {
+    await microphone.toggle()
+  }
+  
+  async function cameraToggle() {
+    await camera.toggle()
+  }
+  
+  async function endCall() {
+    try {
+      await call.endCall()
+      router.push("/")
+    } catch (error) {
+      console.log(`An error occurred: ${error}`)
+      console.log(`Leaving call instead`)
+      await call.leave()
+      router.push("/")
+    }
+  }
+  
+  return (
+    <div className="flex gap-x-2.5 items-center">
+      <Button size="lg" className="rounded-full cursor-pointer border border-green-300" variant="outline">
+        <span className="font-sans text-base font-medium">Good</span>
+      </Button>
+      <div>
+        {isCameraMute ? <Button size="icon-lg" className="rounded-full cursor-pointer" variant="default" onClick={cameraToggle}>
+          <Video />
+        </Button>
+        :
+        <Button size="icon-lg" className="rounded-full cursor-pointer" variant="destructive" onClick={cameraToggle}>
+          <VideoOff />
+        </Button>}
+      </div>
+      <div>
+        {isMute ? <Button size="icon-lg" className="rounded-full cursor-pointer" variant="default" onClick={microphoneToggle}>
+          <Mic />
+        </Button>
+        :
+        <Button size="icon-lg" className="rounded-full cursor-pointer" variant="destructive" onClick={microphoneToggle}>
+          <MicOff />
+        </Button>}
+      </div>
+      <Button size="icon-lg" className="rounded-full cursor-pointer" variant="default">
+        <PictureInPicture2 />
+      </Button>
+      <Button size="icon-lg" className="cursor-pointer rounded-[30px]" variant="destructive" aria-label="End Video call">
+        <PhoneOff />
+      </Button>
+    </div>
+  )
+}
+
 export function MyParticipantList({ participants }: { participants: StreamVideoParticipant[] }) {
   return (
     <div className="h-[90vh] w-[90vw] border border-black rounded-2xl">
-      {participants.map(participant => (
+      {participants.slice(0, 1).map(participant => (
         <ParticipantView key={participant.sessionId} participant={participant} />
       ))}
     </div>
